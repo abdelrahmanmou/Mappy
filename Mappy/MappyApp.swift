9b2272339f912466be8097ed4ce58f05d9a38b80
Abdelrahman <abdo.moustafa.1998@gmail.com>
2023-03-10 14:16:04 +0200

    Initial Commit

---
+++ Mappy/MappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappyApp.swift
+//  Mappy
+//
+//  Created by Abdelrahman Moustafa on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
